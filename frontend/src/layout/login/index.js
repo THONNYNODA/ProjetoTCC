1d370dfb9358894314ddb01ea16be5264acdc5d2
Thonny Noda <noda.thonny@gmail.com>
2021-07-04 13:16:39 -0300

    Atualualizado em 04-07-2021

--- frontend/src/layout/login/index.js
+++ frontend/src/layout/login/index.js
@@ -1,37 +1,44 @@
 import React from 'react';
 
+
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import {loginSyles} from './styles';
 import logoImg from '../../assets/logo.png'
+
+import {loginSyles, Imput, Title, Text, BackBox, BoxText} from './styles';
+
 
 function login() {
   const classes = loginSyles();
 
   return(
-    <div className={classes.wrapper}>
-      <Grid container xs={12} className={classes.wrapperLogin} > 
-      
-        <Paper className={classes.loginItem}>
+    <div className={classes.wrapperContainer}>
+      <BackBox />
+      <Grid container xs={12} className={classes.wrapperLogin} >       
+        <Paper className={classes.boxLogin}>
         <div>
           <img className={classes.logoSize} src={logoImg}/>
-          <h2>
+          <Title>
             Acesse ao S.O.S
-          </h2>
+          </Title>
         </div>
         <form className={classes.formSize}   noValidate autoComplete="off">
-          <div>
-          <TextField id="standard-basic" label="Login" />
-          </div>
-          <div>
-          <TextField type="password" id="standard-basic" label="Senha" />
-          </div>
+          <Imput>
+            <TextField fullWidth  id="standard-basic" label="Login" />
+          </Imput>         
+          <Imput>
+            <TextField fullWidth type="password" id="standard-basic" label="Senha" />
+          </Imput>         
         </form>
-        <p>Esqueceu a senha?</p>
-        <Button variant="contained" color="primary">
-          Enviar
+        <BoxText>
+          <Text>Esqueceu
+             a senha?</Text>
+          <Text>Não sou Cadastrado(a)</Text>
+        </BoxText>
+        <Button className={classes.buttom}>
+          Entrar
         </Button>
         </Paper>
       </Grid>
--- frontend/src/layout/login/styles.js
+++ frontend/src/layout/login/styles.js
@@ -1,4 +1,54 @@
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, styled } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+
+import BackgroundImg from '../../assets/background.jpg'
+//import styled from 'styled-components';
+
+ export const Imput = styled(Box)({
+  margin:'10px',
+  '& label.Mui-focused': {
+    color: '#1FA774',
+  },
+  '& .MuiInput-underline:after': {
+    borderBottomColor: '#1FA774',
+  },
+  '&.Mui-focused fieldset': {
+    borderColor: '#1FA774',
+  },
+ });
+
+export const BackBox = styled(Box)({
+  width: "100%",
+  height: "100vh",
+  position: 'absolute',
+  filter:'blur(5px)',
+  backgroundImage: `url(${BackgroundImg})`,
+  backgroundSize:'cover',
+  backgroundRepeat:'no-repeat',
+});
+
+export const BoxText = styled(Box)({
+  display: 'flex',
+  margin: "10px 0",
+  justifyContent:'space-around',
+});
+
+export const Title = styled(Typography)({
+  padding: 5,
+  color: '#5f615f',
+});
+
+export const Text = styled(Typography)({
+  margin: '10px',
+  fontSize:"12px",
+  color:'#5f615f',
+  cursor: 'pointer',
+  '&:hover':{
+    color:'#1FA774',
+    textDecoration: 'underline',
+  }
+});
 
 export const loginSyles = makeStyles({
     logoSize:{
@@ -6,24 +56,34 @@ export const loginSyles = makeStyles({
       height:90,
     },
     formSize:{
-      margin:"15px 0",
+      margin:"15px 0 30px 0",
     },
 
-    wrapper: {
+    wrapperContainer: {
       width: "100%",
-      height: "100vh",        
-      backgroundColor: "#e8e8e8",
-      backgroundImage: "linear-gradient(0deg, #e8e8e8 0%, #7af9c6 100%)",
-      
+      height: "100vh",      
     },
     wrapperLogin:{
         display:'Grid',
         placeItems:'center',
-        height:'100vh'
+        height:'100vh',
     },
-    loginItem:{
-      backgroundColor: 'rgba(232, 232, 232,0.7)',
+    boxLogin:{
+      width: 350,
+      backgroundColor: '#e8e8e8',
       padding: 30,
-      textAlign:'center'
+      textAlign:'center',
+      zIndex: 999
   },
+    buttom:{
+      width: "100%",
+      fontSize:'1.2rem',
+      background: '#1FA774',
+      border: '3px solid #1FA774',
+      color: '#e8e8e8',
+      '&:hover':{
+        color:'#1FA774',
+        
+      }
+    }
   });
